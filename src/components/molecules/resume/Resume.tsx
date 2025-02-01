044f57ef1798ced82819986c3533d7dbbfd1b052
Tayyyyylor <bryan.h.pro@gmail.com>
2025-02-01 13:10:25 +0100

    change flip of place

--- src/components/molecules/resume/Resume.tsx
+++ src/components/molecules/resume/Resume.tsx
@@ -2,6 +2,7 @@ import Title from '@/components/atoms/title/Title'
 import { useTranslations } from 'next-intl'
 import React from 'react'
 import styles from './Resume.module.scss'
+import FlipImage from '../flipImage/FlipImage'
 
 export default function Resume() {
     const t = useTranslations('Resume')
@@ -9,6 +10,7 @@ export default function Resume() {
     return (
         <article className={styles.resume} id="resume">
             <Title label={t('title')} text="CocoA-Bio" />
+            <FlipImage frontSrc="/2.png" backSrc="/lastImg.png" alt="" />
             <div className={styles.textContainer}>
                 <p className={styles.paragraph}>{t('paraph1')}</p>
                 <p className={styles.paragraph}>{t('paraph2')}</p>
